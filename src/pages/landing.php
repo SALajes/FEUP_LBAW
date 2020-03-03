5d4e8d8a54551a43d57aee460ab7645f1368acb1
Carlos Eduardo da Nova Duarte <cadu@zen.lan>
2020-03-03 18:30:15 +0000

    Hello world

---
+++ src/pages/landing.php
@@ -0,0 +1,86 @@
+<?php
+include_once('../templates/header.php');
+?>
+<div class="background-gradient-blue">
+  <header class="masthead mb-auto pt-3 d-flex pr-5 justify-content-end">
+    <div class="inner">
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active text-white" href="#">Home</a>
+        <a class="nav-link text-white" href="#">Features</a>
+        <a class="nav-link text-white" href="#">Contact</a>
+      </nav>
+    </div>
+  </header>
+
+  <div class="position-relative overflow-hidden p-3 p-md-5 text-center text-white">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+      <h1 class="display-4 font-weight-normal">LBrAWl</h1>
+      <p class="lead font-weight-normal">The most effective communication experience for university students. Chat, share course materials and form groups - all in one platform.</p>
+      <a class="btn btn-outline-light" href="#">Sign up</a>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  </div>
+</div>
+
+
+<div class="container marketing mt-5">
+
+  <!-- START THE FEATURETTES -->
+
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">Sharing and obtaining course materials. <span class="text-info">Simplified.</span></h2>
+      <p class="lead">Each curricular unit has its own drive with course materials uploaded by students. Creating, cooperating
+        and sharing with your peers has never been this easy.
+      </p>
+    </div>
+    <div class="col-md-5">
+      <i class="fa fa-download text-gradient-blue" style="font-size: 13rem;"></i>
+    </div>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7 order-md-2">
+      <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5 order-md-1">
+      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500">
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+      </svg>
+    </div>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5">
+      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500">
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+      </svg>
+    </div>
+  </div>
+
+  <hr class="featurette-divider mt-3">
+
+  <!-- /END THE FEATURETTES -->
+
+</div>
+
+<div class="col text-center">
+  <p class="pb_font-14">© 2019 <a href="https://uicookies.com/">Landing</a> Free Bootstrap4. All Rights Reserved. <br> Designed &amp; Developed by <a href="https://uicookies.com/">uicookies.com</a> <small>(Don't remove credit link on this footer. See <a href="https://uicookies.com/license/" target="_blank">license</a>)</small></p>
+  <p class="pb_font-14">Demo Images: <a href="https://unsplash.com/" target="_blank" rel="nofollow">Unsplash</a></p>
+</div>
+
+<?php
+include_once('templates/footer.php');
+?>
