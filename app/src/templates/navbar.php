35f6215c7639c8222adeba284d6419b6c335ff56
SALajes <sofialajes98@gmail.com>
2020-03-07 16:28:08 +0000

    Responsive navbar

--- app/src/templates/navbar.php
+++ app/src/templates/navbar.php
@@ -4,16 +4,15 @@
     <nav id="header" class="navbar d-flex justify-content-between fixed-top navbar-expand-lg navbar-dark">
         <a class="navbar-brand" href="#"><i id="logo" class="icon-logo align-middle"></i></a>
        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#icons_navbarcollapse" aria-controls="navbarCollapse" aria-expanded="true" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="true" aria-label="Toggle navigation">
+            <i class="icon-menu"></i>
         </button>
-
-        <form id="search" class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search">
-            <button id="search" type="button" class="btn btn-light"><i class="icon-search"></i></button>
-        </form>
         
-        <div id="icons_navbarcollapse" >
+        <div id="navbarCollapse" class="collapse navbar-collapse" >
+            <form id="search" class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                <button type="button" class="btn btn-light"><i class="icon-search"></i></button>
+            </form>
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="icon-notification align-middle"></i></a>
