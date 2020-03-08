e39d84da73949d9192c93c223c327fb37408ddf2
carlosnovaduarte <carlosnovaduarte@outlook.com>
2020-03-08 16:18:26 +0000

    Added search page

---
+++ app/src/pages/search.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="../styles/homepage.css">
+
+<?php
+include_once('../templates/header.php');
+
+include_once('../templates/navbar.php');
+?>
+
+<div class="container-fluid">
+  <div id="homepage" class="row justify-content-md-center">
+    <div class="col-3 d-none d-md-block">
+      <?php include_once('../templates/search_bar.php'); ?>
+    </div>
+    <div id="posts" class="col-6">
+      <h1 class="d-block d-md-none text-center" style="padding-bottom: 1rem">Advanced search</h1>
+
+      <?php include('../templates/card_teacher.php'); ?>
+      <?php include('../templates/card_cu.php'); ?>
+      <?php include('../templates/card.php'); ?>
+    </div>
+    <div class="col-3">
+    </div>
+  </div>
+</div>
+
+<?php
+include_once('../templates/footer.php');
+?>
---
+++ app/src/templates/search_bar.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="../styles/homepage_bar.css">
+
+<div id="left_bar" class="d-flex text-wrap flex-column justify-content-center align-items-left">
+    <h2 class="float-left d-block text-truncate">Advanced search</h2>
+
+    <div class="form-check mt-5 py-3">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            My CUs
+        </label>
+    </div>
+
+    <div class="form-check py-3">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            All CUs
+        </label>
+    </div>
+
+    <div class="form-check py-3">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            Teachers
+        </label>
+    </div>
+
+    <div class="form-check py-3">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            Students
+        </label>
+    </div>
+
+    <label for="customRange2" class="py-3">Curricular year</label>
+    <input type="range" class="custom-range" min="1" max="5" id="customRange2">
+</div>
