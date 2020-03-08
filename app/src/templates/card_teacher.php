f303a32ee64a4e88fe902b5ee8791750cdc57812
SALajes <sofialajes98@gmail.com>
2020-03-08 23:17:40 +0000

    fixed footer's position and pictures

--- app/src/templates/card_teacher.php
+++ app/src/templates/card_teacher.php
@@ -3,9 +3,7 @@
 
 <div class="card card-margins">
     <div class="card-header d-flex justify-content-start">
-        <div class="col-3 d-flex">
-            <img src="../../assets/ctl.jpg" class="img-thumbnail" width="100" height="100" />
-        </div>
+            <img src="../../assets/ctl.jpg" class="img-thumbnail" style="border-radius: 50%; height: 5rem;" />
         <div class="col-9 flex-column">
             <a href="#"><i class="icon-user"></i> Carla Teixeira Lopes</a>
             <a href="#"><i class="icon-ellipsis"></i></a>
--- app/src/templates/footer.php
+++ app/src/templates/footer.php
@@ -1,4 +1,6 @@
-    <footer class="col text-center">
-    <p style="font-size: small; padding-top: 1rem;">© 2020 LBrAWlers. All Rights Reserved.</p>
+<link rel="stylesheet" href="../styles/footer.css">
+
+    <footer>
+    <p>© 2020 LBrAWlers. All Rights Reserved.</p>
     </footer>
 </html>
