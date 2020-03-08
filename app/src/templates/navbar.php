724e2aa8fef381b689cc30ac0bf4c5988f207175
SALajes <sofialajes98@gmail.com>
2020-03-08 00:31:35 +0000

    navigation bar completed

--- app/src/templates/navbar.php
+++ app/src/templates/navbar.php
@@ -9,6 +9,10 @@
         </button>
         
         <div id="navbarCollapse" class="collapse navbar-collapse" >
+            <section id="collapsed_profile" class="d-md-none d-flex flex-row justify-content-center align-items-center flex-wrap">
+                <a class="nav-link" href="#"><i class="icon-user align-middle"></i></a>
+                <section class="d-flex flex-column"><span> John doe </span><span> up000000000 </span><span> #mieic </span></section>
+            </section>
             <form id="search" class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search">
                 <button type="button" class="btn btn-light"><i class="icon-search"></i></button>
@@ -16,19 +20,31 @@
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="icon-notification align-middle"></i></a>
-                    <span class="d-lg-none"> Notifications</span>
+                    <span class="d-md-none"> Notifications</span>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="icon-message align-middle"></i></a>
-                    <span class="d-lg-none"> Messages</span>
+                    <span class="d-md-none"> Messages</span>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="icon-user align-middle"></i></a>
-                    <span class="d-lg-none"> Profile</span>
+                    <span class="d-md-none"> Profile</span>
+                </li>
+                <li class="nav-item d-md-none">
+                    <a class="nav-link" href="#"><i class="icon-cu align-middle"></i></i></a>
+                    <span> My CU's</span>
+                </li>
+                <li class="nav-item d-md-none">
+                    <a class="nav-link" href="#"><i class="icon-manage_cu align-middle"></i></a>
+                    <span> Manage CU's</span>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="icon-question align-middle"></i></a>
-                    <span class="d-lg-none"> Help</span>
+                    <span class="d-md-none"> Help</span>
+                </li>
+                <li class="nav-item d-md-none">
+                    <a class="nav-link" href="#"><i class="icon-log-out align-middle"></i></a>
+                    <span> Logout</span>
                 </li>
             </ul>
         </div>
