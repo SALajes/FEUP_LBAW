972f3610d7bcf93d6c3f1a2c1598e4badd6f3171
pedrocarmopereira <pppereira3@gmail.com>
2020-03-11 20:21:55 +0000

    tutoring page

--- app/src/templates/card.php
+++ app/src/templates/card.php
@@ -72,3 +72,39 @@
         </div>
     </div>
 <?php } ?>
+
+<?php function draw_card_tutor1() { ?>
+    <div class="card card-margins">
+        <div class="card-header d-flex justify-content-between">
+            <a href="#"><i class="icon-user"></i> João Correia</a>
+            <a href="#"><i class="icon-ellipsis"></i></a>
+        </div>
+
+        <div class="card-body">
+            Procuro, explicador jovem, musculado, de mais de 18 anos, preferencialmente latino e de olhos verdes, para explicações de Bases de Dados.
+        </div>
+
+        <div class="card-footer">
+            <a href="#" class="comments-padding">5 comments</a>
+            <a href="#">3 likes</a>
+        </div>
+    </div>
+<?php } ?>
+
+<?php function draw_card_tutor2() { ?>
+    <div class="card card-margins">
+        <div class="card-header d-flex justify-content-between">
+            <a href="#"><i class="icon-user"></i> Carla Explicadora</a>
+            <a href="#"><i class="icon-ellipsis"></i></a>
+        </div>
+
+        <div class="card-body">
+           Se me pagarem passo-vos.
+        </div>
+
+        <div class="card-footer">
+            <a href="#" class="comments-padding">999 comments</a>
+            <a href="#">999 likes</a>
+        </div>
+    </div>
+<?php } ?>
