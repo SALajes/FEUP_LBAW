e6bf301ec889ea8d8dc1d2bc1234a4b0d27c7b80
pedrocarmopereira <pppereira3@gmail.com>
2020-03-11 20:28:41 +0000

    Classes

--- app/src/actions/action_classes.php
+++ app/src/actions/action_classes.php
@@ -1,63 +1,75 @@
-<?php
-echo "<p>
+<table class="table table-striped">
+	<h4>Class 1</h4>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Carlos</td>
+      <td>Nova</td>
+      <td>12345678</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Pedro</td>
+      <td>Pereira</td>
+      <td>91011121</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Simão</td>
+      <td>Oliveira</td>
+      <td>31415161</td>
+	</tr>
+	<tr>
+      <th scope="row">4</th>
+      <td>Sofia</td>
+      <td>Lages</td>
+      <td>71819202</td>
+    </tr>
+  </tbody>
+</table>
 
-orem ipsum,
- dolor sit amet consectetur adipisicing elit.
-  Nulla libero, incidunt consequatur aliquam voluptatum aspern
-  atur cum qui rerum, hic natus doloribus quam nesciunt ab neque
-   magnam accusamus, deserunt ratione impedit nam dolorum? Fugiat
-   , iste cupiditate praesentium dicta voluptate inciduntorem ipsum,
-   dolor sit amet consectetur adipisicing elit.
-	Nulla libero, incidunt consequatur aliquam voluptatum aspern
-	atur cum qui rerum, hic natus doloribus quam nesciunt ab neque
-	 magnam accusamus, deserunt ratione impedit nam dolorum? Fugiat
-	 , iste cupiditate praesentium dicta voluptate inciduntorem ipsum,
-	 dolor sit amet consectetur adipisicing elit.
-	  Nulla libero, incidunt consequatur aliquam voluptatum aspern
-	  atur cum qui rerum, hic natus doloribus quam nesciunt ab neque
-	   magnam accusamus, deserunt ratione impedit nam dolorum? Fugiat
-	   , iste cupiditate praesentium dicta voluptate inciduntFIRST MURDERER
-
-     (Stabbing him)     What, you egg?
-Young fry of treachery!
-FIRST MURDERER
-
-What’s that, you runt? (stabbing him) Young son of a traitor!
-SON
-
-80He has killed me, mother.
-Run away, I pray you!
-SON
-
-He has killed me, Mother. Run away, I beg you!
-He dies. Exit LADY MACDUFF, crying “Murder!” followed by MURDERERS
-Enter MALCOLM and MACDUFF
-MALCOLM and MACDUFF enter.
-MALCOLM
-
-Let us seek out some desolate shade and there
-Weep our sad bosoms empty.
-MALCOLM
-
-Let’s seek out some shady place where we can sit down alone and cry our hearts out.
-MACDUFF
-
-     Let us rather
-Hold fast the mortal sword and, like good men,
-Bestride our downfall'n birthdom. Each new morn
-5New widows howl, new orphans cry, new sorrows
-Strike heaven on the face, that it resounds
-As if it felt with Scotland and yelled out
-Like syllable of dolALCOLM
-
-     What I believe I’ll wail;
-What know believe, and what I can redress,
-10As I shall find the time to friend, I will.
-What you have spoke, it may be so perchance.
-This tyrant, whose sole name blisters our tongues,
-Was once thought honest. You have loved him well.
-He hath not touched you yet. I am young, but something
-15You may deserve of him through me, and wisdom
-To offer up a weak, poor, innocent lamb
-T' appease an angry god.</p>"
-?>
+<table class="table table-striped">
+	<h4>Class 2</h4>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Zé</td>
+      <td>Ninguém</td>
+      <td>0000000</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Fedro</td>
+      <td>Fereira</td>
+      <td>11011121</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Banana</td>
+      <td>Rama</td>
+      <td>51241161</td>
+	</tr>
+	<tr>
+      <th scope="row">4</th>
+      <td>Donald</td>
+      <td>Drumpf</td>
+      <td>718192124</td>
+    </tr>
+  </tbody>
+</table>
