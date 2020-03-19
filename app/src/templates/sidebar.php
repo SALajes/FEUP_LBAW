43362f92ee013b145f7617c4ca1665435b5bf355
pedrocarmopereira <pppereira3@gmail.com>
2020-03-19 20:41:55 +0000

    Sidebar homegenization

--- app/src/templates/sidebar.php
+++ app/src/templates/sidebar.php
@@ -22,19 +22,19 @@
             <h4 class="text-center">My CU's</h4>
             <ul>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                    COMP
+                    <a href="cupage.php"> COMP </a>  
                     <span class="badge badge-primary badge-pill">14</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                    IART
+                <a href="cupage.php"> IART </a>
                     <span class="badge badge-primary badge-pill">2</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                    LBAW
+                <a href="cupage.php"> LBAW</a>
                     <span class="badge badge-primary badge-pill">1</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                    PPIN
+                <a href="cupage.php"> BDAD </a>
                     <span class="badge badge-primary badge-pill">1</span>
                 </li>
             </ul>
@@ -59,39 +59,51 @@
             <div>
                 <i class="icon-add_friend" style="color: #0aedb3"></i>
             </div>
-        </div>md
+        </div>
     </section>
     </aside>
 
     <!-- Divisao Vertical -->
 <?php } ?>
 
-<?php function draw_sidebar_cu()
+<?php function draw_sidebar_CU()
 { ?>
 
     <section class="d-lg-block offset-lg-6 offset-xl-1 d-flex justify-content-center flex-wrap">
         <div class="btn-group-vertical btn-group-toggle d-flex flex-wrap justify-content-center" role="group" aria-label="Tabs" id="cu_tabs">
             <div class="row col-xl-12 col-md-4 col-6 justify-content-center">
-            <button id="feed_btn" type="button" class="btn btn-link"><h3>Feed</h3></button>
+                <button id="feed_btn" type="button" class="btn btn-link">
+                    <h3>Feed</h3>
+                </button>
             </div>
             <div class="row col-xl-12 col-md-4 col-6 justify-content-center">
-            <button id="drive_btn" type="button" class="btn btn-link"><h3>Drive</h3></button>
+                <button id="drive_btn" type="button" class="btn btn-link">
+                    <h3>Drive</h3>
+                </button>
             </div>
             <div class="row col-xl-12 col-md-4 col-6 justify-content-center">
-            <button id="doubts_btn" type="button" class="btn btn-link"><h3>Doubts</h3></button>
+                <button id="doubts_btn" type="button" class="btn btn-link">
+                    <h3>Doubts</h3>
+                </button>
             </div>
             <div class="row col-xl-12 col-md-4 col-6 justify-content-center">
-            <button id="tutor_btn" type="button" class="btn btn-link"><h3>Tutoring</h3></button>
+                <button id="tutor_btn" type="button" class="btn btn-link">
+                    <h3>Tutoring</h3>
+                </button>
             </div>
             <div class="row col-xl-12 col-md-4 col-6 justify-content-center">
-            <button id="classes_btn" type="button" class="btn btn-link"><h3>Classes</h3></button>
+                <button id="classes_btn" type="button" class="btn btn-link">
+                    <h3>Classes</h3>
+                </button>
             </div>
             <div class="row col-xl-12 col-md-4 col-6 justify-content-center">
-            <button id="about_btn" type="button" class="btn btn-link"><h3>About</h3></button>
+                <button id="about_btn" type="button" class="btn btn-link">
+                    <h3>About</h3>
+                </button>
             </div>
         </div>
     </section>
-    <hr>
+
     <!-- Divisao Vertical -->
     </aside>
 
