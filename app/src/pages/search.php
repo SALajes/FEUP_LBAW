d3a6611c7fbbe86558b7403cdc6617f278b11ab1
pedrocarmopereira <pppereira3@gmail.com>
2020-03-19 21:03:36 +0000

    Almost done with structuring, just have to center all forms and all pages will have same sidebar struct

--- app/src/pages/search.php
+++ app/src/pages/search.php
@@ -1,28 +1,40 @@
 <link rel="stylesheet" href="../styles/homepage.css">
-
 <?php
 include_once('../templates/header.php');
 
 include_once('../templates/navbar.php');
+
+
+include_once('../templates/card.php');
+
+include_once('../templates/sidebar.php');
 ?>
 
-<div class="container-fluid">
-  <div id="homepage" class="row justify-content-md-center">
-    <div class="col-3 d-none d-md-block">
-      <?php include_once('../templates/search_bar.php'); ?>
-    </div>
-    <div id="posts" class="col-6">
-      <h1 class="d-block d-md-none text-center" style="padding-bottom: 1rem">Search</h1>
+<script src="../scripts/cu_sidebar.js" defer></script>
+<body id="cupage" class="container-fuild ">
+    
+
+        <div class="row">
+            <?php
+            draw_sidebar_Top("BDAD", "Alvaro Campos", "up188800613", "");
+            draw_sidebar_Search();
+            ?>
+            <main id="posts" class="col-lg-6 col-md-12">
+
+                <div id="content" class="col-12 offset-lg-0">
 
       <?php include('../templates/card_teacher.php'); ?>
       <?php include('../templates/card_cu.php'); ?>
       <?php include('../templates/card.php'); ?>
-    </div>
-    <div class="col-3">
-    </div>
-  </div>
-</div>
 
+                </div>
+            </main>
+        <div>
+
+        <div class="col-3">
+        </div>
+
+</body>
 <?php
 include_once('../templates/footer.php');
 ?>
--- app/src/templates/sidebar.php
+++ app/src/templates/sidebar.php
@@ -108,3 +108,49 @@
     </aside>
 
 <?php } ?>
+
+<?php function draw_sidebar_Search(){ ?>
+
+    <section class="row-md-auto justify-content-center">
+        
+    <div id="left_bar" class="d-flex text-wrap flex-column justify-content-center">
+    <h2 class="float-left d-block text-truncate text-center">Search</h2>
+
+    <div class="form-check mt-5 py-3">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            My CUs
+        </label>
+    </div>
+
+    <div class="form-check py-3">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            All CUs
+        </label>
+    </div>
+
+    <div class="form-check py-3">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            Teachers
+        </label>
+    </div>
+
+    <div class="form-check py-3">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            Students
+        </label>
+    </div>
+
+    <label for="customRange2" class="py-3 text-center">Curricular year</label>
+    <input type="range" class="custom-range" min="1" max="5" id="customRange2">
+</div>
+    </section>
+
+<!-- Divisao Vertical -->
+</aside>
+<?php
+}
+?>
