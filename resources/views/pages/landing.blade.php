5898b9eb1c88e2035a9cb3364b8d78ad3d7c40a6
Carlos Nova Duarte <carlosnovaduarte@outlook.com>
2020-04-18 17:09:20 +0100

    Added modal

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -26,8 +26,38 @@
           The most effective communication experience for university students.
           Chat, share course materials and form groups. All in one platform.
         </p>
+
+        
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
         <a class="btn btn-outline-light" href="homepage.php">Sign up</a>
-        <a class="btn btn-outline-light" href="homepage.php">Login</a>
+        <a id="login_btn" class="btn btn-outline-light" href="homepage.php">Login</a>
       </div>
       <div class="product-device shadow-sm d-none d-md-block"></div>
       <div
