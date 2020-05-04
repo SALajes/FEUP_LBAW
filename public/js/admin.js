8d7c496fd6b1003ce066a46ad478cffdd5900ab7
pedrocarmopereira <pppereira3@gmail.com>
2020-05-04 18:27:36 +0100

    Table representation of all CUs for Admins

--- public/js/admin.js
+++ public/js/admin.js
@@ -11,21 +11,20 @@ function getAllCUs(){
         if(req.status >= 200 && req.status < 400){ // Se o SRV retornar bem
             let cu_list = JSON.parse(this.responseText);
             console.log(cu_list);
-            // for(let i = 0; i < cu_list.cus.length; i++){
-            //     cu_data += "<tr>";
-            //     cu_data += "<tr>";
-            //     cu_data += "<td>";
-            //     cu_data += "<a href=\"/cu/" + cu_list.cus[i].id + "\">" + cu_list.cus[i].abbrev + "</a>";
-            //     cu_data += "</td>";
-            //     cu_data += " <td>Enrolled</td>";
-            //     cu_data += "<td>Leave</td>";
-            //     cu_data+= "</tr>";
-            // }
+            for(let i = 0; i < cu_list.cus.length; i++){
+                cu_data += "<tr>";
+                cu_data += "<tr>";
+                cu_data += "<td>";
+                cu_data += "<a href=\"/cu/" + cu_list.cus[i].id + "\">" + cu_list.cus[i].abbrev + "</a>";
+                cu_data += "</td>";
+                cu_data += "<td>Manage</td>";
+                cu_data+= "</tr>";
+            }
 
-            // data.innerHTML = "<section class=\"row\"><table class=\"table text-center table-hover\"><thead><tr><th scope=\"col\">Name</th><th scope=\"col\">Status</th><th scope=\"col\">Action</th></tr></thead><tbody>" + cu_data + "</tbody></table></section>";
+            data.innerHTML = "<section class=\"row\"><table class=\"table text-center table-hover\"><thead><tr><th scope=\"col\">Name</th></th><th scope=\"col\">Action</th></tr></thead><tbody>" + cu_data + "</tbody></table></section>";
         }
-        
-        if (req.status == 404) console.log(404);
+		
+		else console.log(req.status);
     };
     
     req.onerror = function (){ //SE não ligar ao srv
