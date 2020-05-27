57669341ebf37bc7f1b0f623dfcfa76bbf4657d7
Carlos Nova Duarte <cnova1998@gmail.com>
2020-05-27 15:58:38 +0100

    Removed spaghetti

--- public/js/admin.js
+++ public/js/admin.js
@@ -18,55 +18,37 @@ function getAllCUs(){
             let cu_list = JSON.parse(this.responseText);
             console.log(cu_list)
             counter = 0;
-            let student_counter = 1;
-            let current_cu_abbrev = cu_list.cus[0].abbrev;
-            let current_cu_name = cu_list.cus[0].cu_name;
-            let current_cu_description = cu_list.cus[0].description;
-            let current_cu_id = cu_list.cus[0].cu_id;
-            for(let i = 0; i < cu_list.cus.length; i++) {
-                current_cu_abbrev = cu_list.cus[i].abbrev;
-                current_cu_name = cu_list.cus[i].cu_name;
-                current_cu_description = cu_list.cus[i].description;
-                current_cu_id = cu_list.cus[i].cu_id;
+            for (let i = 0; i < cu_list.cus.length; i++) {
+                let current_cu_abbrev = cu_list.cus[i].abbrev;
+
                 if (!(current_cu_abbrev in aux_map)) {
                     aux_map[current_cu_abbrev] = 1;
                 }
-                if (i+1 < cu_list.cus.length && current_cu_abbrev == cu_list.cus[i+1].abbrev) {
-                    student_counter++;
+                else {
                     aux_map[current_cu_abbrev]++;
                 }
-                else {
-                    if (!cu_abbrevs.includes(current_cu_abbrev)) {
-                        if (student_counter == 0 && cu_list.cus[i].student_number != null) {
-                            student_counter = 1;
-                            aux_map[current_cu_abbrev] = 1;
-                        }
-                        else if (cu_list.cus[i].student_number != null) {
-                            student_counter++;
-                            aux_map[current_cu_abbrev]++;
-                        }
-                        cu_abbrevs.push(current_cu_abbrev);
-                        cu_data += "<tr>";
-                        cu_data += "<tr>";
-                        cu_data += "<td>";
-                        cu_data += "<a href=\"/cu/" + current_cu_id + "\">" + current_cu_abbrev + "</a>";
-                        cu_data += "</td>";
-                        cu_data += "<td>" + current_cu_name + "</td>";
-                        cu_data += "<td>";
-                        cu_data += aux_map[current_cu_abbrev];
-                        cu_data += "</td>";
-                        cu_data+= "</tr>";
-                    }
-                    if (i+1 < cu_list.cus.length && current_cu_abbrev != cu_list.cus[i+1].abbrev) 
-                        student_counter = 0;
-                }             
             }
-
+            for (let i = 0; i < cu_list.cus.length; i++) {
+                let current_cu_abbrev = cu_list.cus[i].abbrev;
+                let current_cu_id = cu_list.cus[i].id;
+                let current_cu_name = cu_list.cus[i].name;
+                if (!cu_abbrevs.includes(current_cu_abbrev)) {
+                    cu_abbrevs.push(current_cu_abbrev);
+                    cu_data += "<tr>";
+                    cu_data += "<tr>";
+                    cu_data += "<td>";
+                    cu_data += "<a href=\"/cu/" + current_cu_id + "\">" + current_cu_abbrev + "</a>";
+                    cu_data += "</td>";
+                    cu_data += "<td>" + current_cu_name + "</td>";
+                    cu_data += "<td>";
+                    cu_data += aux_map[current_cu_abbrev];
+                    cu_data += "</td>";
+                    cu_data+= "</tr>";
+                }
+            }
             data.innerHTML = "<section class=\"row\"><table class=\"table text-center table-hover\"><thead><tr><th scope=\"col\">Abbreviature</th></th><th scope=\"col\">Name</th><th scope=\"col\">Number of enrolled students</th></tr></thead><tbody>" + cu_data + "</tbody></table></section>";
         }		
-        else console.log(req.status);
-        console.log(aux_map)
-        
+        else console.log(req.status);        
     };
     
     req.onerror = function (){ //SE não ligar ao srv
