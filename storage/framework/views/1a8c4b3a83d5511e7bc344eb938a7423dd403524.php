1d61197284e065aeeaa6cbd3612a9b30dd4f9f0a
pedrocarmopereira <pppereira3@gmail.com>
2020-05-30 20:05:50 +0100

    Removes storage from staging area

--- storage/framework/views/1a8c4b3a83d5511e7bc344eb938a7423dd403524.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="<?php echo e(asset('css/footer.css')); ?>">
-
-    <footer>
-    <p>© 2020 LBrAWlers. All Rights Reserved.</p>
-    </footer>
-<?php /**PATH /home/cadu/Git/lbaw2013/resources/views/partials/footer.blade.php ENDPATH**/ ?>
