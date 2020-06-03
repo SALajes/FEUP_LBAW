94352c609647d16d83b98cad2072dac3d4906e56
Simawatt <simaoxo@hotmail.com>
2020-06-03 15:17:34 +0100

    more fixes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -186,7 +186,7 @@
 		<!-- Divisao Vertical -->
 	</aside>
 	<script src={{ asset('js/editCU.js') }} defer></script>
-	<div class="modal fade" id="editProfileModal" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal fade" id="editCUModal" tabindex="-1" role="dialog" aria-hidden="true">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
